c7141a9ba8ac297f6d43c699481db046aef85066
Grace Gu <grace.gu@leanplum.com>
2018-11-08 14:22:45 -0800

    target source sdk

--- app/src/main/java/com/leanplum/rondo/Configure.java
+++ app/src/main/java/com/leanplum/rondo/Configure.java
@@ -15,11 +15,11 @@
 package com.leanplum.rondo;
 
 class Configure {
-  static final String APP_ID = "app_ve9UCNlqI8dy6Omzfu1rEh6hkWonNHVZJIWtLLt6aLs";
-  static final String PRODUCTION_KEY = "prod_D5ECYBLrRrrOYaFZvAFFHTg1JyZ2Llixe5s077Lw3rM";
-  static final String DEVELOPMENT_KEY = "dev_cKF5HMpLGqhbovlEGMKjgTuf8AHfr2Jar6rrnNhtzQ0";
+  static final String APP_ID = "app_xJYMmZMZRZjsgOabM950Zvq0J9K9RU7ub1sgDTlqdYE";
+  static final String PRODUCTION_KEY = "prod_HArP7HTQ7HhPnXOagKPKQKbD3IuOFrsPjuRcyhegYF8";
+  static final String DEVELOPMENT_KEY = "dev_Ap7Him1JNSAgIK8SxIBNPuff7WXSYWOjXQklnuoAyV4";
 
-  static final String API_HOST_NAME = "www.leanplum.com";
+  static final String API_HOST_NAME = "leanplum-staging.appspot.com";
   static final Boolean API_SSL = true;
 
   static final String SOCKET_HOST_NAME = "dev.leanplum.com";
