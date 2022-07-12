62efb61a2d45bb403cb1d3f63f841e06e0b7059e
Hristo Borisov <hristo.borisov@leanplum.com>
2022-07-12 18:49:23 +0300

    Create activity with layout

---
+++ RondoApp/src/main/java/com/leanplum/rondo/QueueActivity.kt
@@ -0,0 +1,13 @@
+package com.leanplum.rondo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QueueActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_queue)
+  }
+
+}
